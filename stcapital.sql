961c35e29253ee65ecb9edf98c01affd5253a649
BSTung <43914292+BSTung@users.noreply.github.com>
2022-03-08 14:48:24 +0700

    Add files via upload

---
+++ stcapital.sql
@@ -0,0 +1,343 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th3 08, 2022 lúc 08:19 AM
+-- Phiên bản máy phục vụ: 10.4.13-MariaDB
+-- Phiên bản PHP: 7.4.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `stcapital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `attributes`
+--
+
+CREATE TABLE `attributes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `atb_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `atb_slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `atb_type` tinyint(4) NOT NULL DEFAULT 0,
+  `atb_category_id` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `c_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `c_avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `c_banner` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `c_description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `c_parent_id` int(11) NOT NULL DEFAULT 0,
+  `c_hot` tinyint(4) NOT NULL DEFAULT 0,
+  `c_status` tinyint(4) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `keywords`
+--
+
+CREATE TABLE `keywords` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `k_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `k_slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `k_description` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `k_hot` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2022_03_03_160112_create_categories_table', 1),
+(4, '2022_03_04_041101_create_keywords_table', 1),
+(5, '2022_03_04_104052_create_products_table', 1),
+(6, '2022_03_04_181548_add_pro_country_to_products_table', 1),
+(7, '2022_03_06_143402_create_attributes_table', 1),
+(8, '2022_03_07_064232_create_products_attributes_table', 1),
+(9, '2022_03_07_092055_create_products_keywords_table', 1),
+(10, '2022_03_07_092157_create_product_images_table', 1),
+(11, '2022_03_07_193354_alter_column_c_parent_id_in_categories_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `pro_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pro_slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pro_price` int(11) NOT NULL DEFAULT 0,
+  `pro_price_entry` int(11) NOT NULL DEFAULT 0 COMMENT 'giá nhập',
+  `pro_category_id` int(11) NOT NULL DEFAULT 0,
+  `pro_admin_id` int(11) NOT NULL DEFAULT 0,
+  `pro_sale` tinyint(4) NOT NULL DEFAULT 0,
+  `pro_avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pro_view` int(11) NOT NULL DEFAULT 0,
+  `pro_hot` tinyint(4) NOT NULL DEFAULT 0,
+  `pro_active` tinyint(4) NOT NULL DEFAULT 1,
+  `pro_pay` int(11) NOT NULL DEFAULT 0,
+  `pro_description` mediumtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pro_content` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `pro_review_total` int(11) NOT NULL DEFAULT 0,
+  `pro_review_star` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `pro_country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products_attributes`
+--
+
+CREATE TABLE `products_attributes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `pa_product_id` int(11) NOT NULL DEFAULT 0,
+  `pa_attribute_id` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products_keywords`
+--
+
+CREATE TABLE `products_keywords` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `pk_product_id` int(11) NOT NULL DEFAULT 0,
+  `pk_keyword_id` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `pi_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pi_slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pi_product_id` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `attributes`
+--
+ALTER TABLE `attributes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `attributes_atb_name_unique` (`atb_name`),
+  ADD KEY `attributes_atb_category_id_index` (`atb_category_id`);
+
+--
+-- Chỉ mục cho bảng `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `categories_c_slug_unique` (`c_slug`),
+  ADD KEY `categories_c_parent_id_index` (`c_parent_id`);
+
+--
+-- Chỉ mục cho bảng `keywords`
+--
+ALTER TABLE `keywords`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `products_pro_slug_unique` (`pro_slug`),
+  ADD KEY `products_pro_hot_index` (`pro_hot`),
+  ADD KEY `products_pro_active_index` (`pro_active`);
+
+--
+-- Chỉ mục cho bảng `products_attributes`
+--
+ALTER TABLE `products_attributes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_attributes_pa_product_id_index` (`pa_product_id`),
+  ADD KEY `products_attributes_pa_attribute_id_index` (`pa_attribute_id`);
+
+--
+-- Chỉ mục cho bảng `products_keywords`
+--
+ALTER TABLE `products_keywords`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `products_keywords_pk_product_id_index` (`pk_product_id`),
+  ADD KEY `products_keywords_pk_keyword_id_index` (`pk_keyword_id`);
+
+--
+-- Chỉ mục cho bảng `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`),
+  ADD UNIQUE KEY `users_phone_unique` (`phone`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `attributes`
+--
+ALTER TABLE `attributes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `keywords`
+--
+ALTER TABLE `keywords`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `products_attributes`
+--
+ALTER TABLE `products_attributes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `products_keywords`
+--
+ALTER TABLE `products_keywords`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
